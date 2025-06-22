f8d835800fd67c6252c72bb604435aecf7dbce79
Fiz5892 <156051961+Fiz5892@users.noreply.github.com>
2025-06-22 15:52:10 +0700

    Update mailer.ts

--- app/utils/mailer.ts
+++ app/utils/mailer.ts
@@ -1,26 +1,13 @@
-// import nodemailer from 'nodemailer';
-
-// // Membuat transporter untuk mengirim email
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.zoho.com',
-//   port: 465, // Gunakan port 465 untuk SSL atau 587 untuk TLS
-//   secure: true, // true untuk port 465, false untuk port 587
-//   auth: {
-//     user: 'thriftease@zohomail.com', // Ganti dengan alamat email Zoho Anda
-//     pass: '2AWG91hiB9e2',    // Ganti dengan App Password Zoho Anda
-//   },
-// });
-
-// export default transporter;
-
 import nodemailer from 'nodemailer';
 
-// Buat transporter untuk Gmail
+// Membuat transporter untuk mengirim email
 const transporter = nodemailer.createTransport({
-  service: 'gmail', // Gunakan service Gmail
+  host: 'smtp.gmail.com',
+  port: 465, // Gunakan port 465 untuk SSL atau 587 untuk TLS
+  secure: true, // true untuk port 465, false untuk port 587
   auth: {
-    user: 'thriftease4315@gmail.com', // Ganti dengan alamat Gmail kamu
-    pass: 'zsaf vbtk pxzb gbyu',   // Ganti dengan App Password dari Gmail
+     user: process.env.EMAIL_USER,
+     pass: process.env.EMAIL_PASS     
   },
 });
 
